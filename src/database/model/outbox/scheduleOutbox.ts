ffc0c7933b26079e104e3d64a37785bf92cb9c90
g.coletta <giali.coletta@fullstackagency.it>
2023-09-02 12:50:50 +0200

    Update scheduleOutbox.ts

--- src/database/model/outbox/scheduleOutbox.ts
+++ src/database/model/outbox/scheduleOutbox.ts
@@ -2,7 +2,7 @@ import { Chunk, Context, Duration, Effect, Schedule } from "effect";
 import { GetConnection } from "../../connection";
 import { ModelProvider } from "../model";
 import { getErrorMessage } from "../../../utils";
-import { WithId, Document } from "mongodb";
+import { WithId, Document, Collection } from "mongodb";
 
 export class ScheduleOutboxError extends Error {
   _tag = "ScheduleOutboxError";
@@ -15,39 +15,61 @@ export interface MessageProvider {
 }
 export const MessageProvider = Context.Tag<MessageProvider>();
 
-export const sendOutboxMessages = Effect.gen(function* ($) {
-  const [client, model, messageProvider] = yield* $(
-    Effect.all([GetConnection, ModelProvider, MessageProvider])
-  );
-  const outbox = client.db(model.db).collection(model.collection + "_outbox");
-  const unprocessed = yield* $(
+function* findUnprocessed($: Effect.Adapter, outbox: Collection<Document>) {
+  return yield* $(
     Effect.tryPromise({
       try: () => outbox.find({ processed: false }).toArray(),
       catch: catchError,
     })
   );
+}
+
+function* sendMessage(
+  $: Effect.Adapter,
+  messageProvider: MessageProvider,
+  doc: WithId<Document>
+) {
+  return yield* $(
+    Effect.tryPromise({
+      try: () => messageProvider.send(doc.data),
+      catch: catchError,
+    })
+  );
+}
+
+function* updateOutbox(
+  $: Effect.Adapter,
+  outbox: Collection<Document>,
+  doc: WithId<Document>,
+  response: boolean
+) {
+  return yield* $(
+    Effect.tryPromise({
+      try: () =>
+        outbox.updateOne(
+          { _id: doc._id },
+          { $set: { processed: response, processedAt: new Date() } }
+        ),
+      catch: catchError,
+    })
+  );
+}
+
+export const sendOutboxMessages = Effect.gen(function* ($) {
+  const [client, model, messageProvider] = yield* $(
+    Effect.all([GetConnection, ModelProvider, MessageProvider])
+  );
+  const outbox = client.db(model.db).collection(model.collection + "_outbox");
+  const unprocessed = yield* findUnprocessed($, outbox);
   return yield* $(
     Effect.forEach(unprocessed, (doc) =>
       Effect.gen(function* () {
-        const response = yield* $(
-          Effect.tryPromise({
-            try: () => messageProvider.send(doc.data),
-            catch: catchError,
-          })
-        );
-        yield* $(
-          Effect.tryPromise({
-            try: () =>
-              outbox.updateOne(
-                { _id: doc._id },
-                { $set: { processed: response, processedAt: new Date() } }
-              ),
-            catch: catchError,
-          })
-        );
-        return Chunk.make(response);
+        const response = yield* sendMessage($, messageProvider, doc);
+        const result = yield* updateOutbox($, outbox, doc, response);
+        return result.upsertedId;
       })
-    )
+    ),
+    Effect.map((items) => Chunk.make(items.filter(Boolean)))
   );
 });
 
