4fd84aba2a59c86ede8b5524ce4a1b2ae96f28ab
g.coletta <giali.coletta@fullstackagency.it>
2023-11-11 18:16:42 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { Effect, Schedule } from "effect";
+import { Effect } from "effect";
 import "dotenv/config";
 import { MongoClient } from "mongodb";
 import { DbProvider } from "./database/connection";
 import { ModelProvider } from "./database/model/model";
 import { getMany } from "./database/model/op/request/getMany";
-import { SyncCollection, SyncItem } from "./database/model/watch/sync/sync";
 
 if (!process.env.MONGO_URL) {
   throw new Error("MONGO_URL is required");
@@ -18,18 +17,7 @@ const ModelLive = Effect.provideService(
   ModelProvider,
   ModelProvider.of({ db: "prova", collection: "leads" })
 );
-const SyncItemLive = Effect.provideService(
-  SyncItem,
-  SyncItem.of({
-    db: "prova",
-    sync: {
-      current: "opportunities",
-      path: "lead",
-      external: "leads",
-    },
-  })
-);
 
-const program = SyncCollection.pipe(DbLive, SyncItemLive);
+const program = getMany([]).pipe(DbLive, ModelLive);
 
 Effect.runPromise(program);
